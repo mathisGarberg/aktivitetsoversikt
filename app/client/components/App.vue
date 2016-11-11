aacebce15cb07c23fc92be8fb8d4d87279ab477c
Toni S <toni.sucic@icloud.com>
2016-11-11 18:13:26 +0100

    Add basic navbar

--- app/client/components/App.vue
+++ app/client/components/App.vue
@@ -5,8 +5,39 @@
 
 <template>
     <div id="app">
-        <h1>Aktivitetsoversikt</h1>
-        <hr>
+        <nav class="navbar">
+            <div class="navbar-brand">Heddal IL</div>
+            <div class="navbar-right">
+                <div>Kjetil Røise (Trener)</div>
+            </div>
+        </nav>
         <router-view></router-view>
     </div>
 </template>
+
+<style lang="sass">
+    .navbar {
+        color: white;
+        width: 100%;
+        padding: 40px;
+        box-sizing: border-box;
+        background-color: rgb(255, 85, 84);
+        display: flex;
+        flex-direction: row;
+        /* justify-content: center; */
+        justify-content: space-between;
+        flex-wrap: nowrap;
+
+        .navbar-brand {
+            font-size: 50px;
+            text-transform: uppercase;
+        }
+
+        .navbar-right {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            font-size: 20px;
+        }
+    }
+</style>
--- app/client/components/Home.vue
+++ app/client/components/Home.vue
@@ -6,7 +6,6 @@
 <template>
     <div>
         <h1>Home</h1>
-        <p>test</p>
     </div>
 </template>
 
