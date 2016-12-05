ea2f1d12b98375cfa2da3e0dc77999d4fa7bdd53
Toni Sucic <toni.sucic@icloud.com>
2016-12-05 23:52:34 +0100

    Add new stuff

---
+++ app/server/database/event.js
@@ -0,0 +1,13 @@
+export default function(conn) {
+    return {
+        async findOneById(id) {
+            
+        },
+
+        async add() {
+        },
+
+        async delete(id) {
+        },
+    };
+};
--- sql/create.sql
+++ sql/create.sql
@@ -3,23 +3,56 @@ CREATE TABLE user (
     role_id INTEGER NOT NULL,
     username VARCHAR(30) NOT NULL,
     password VARCHAR(60) NOT NULL,
-    full_name VARCHAR(100) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
     PRIMARY KEY(id),
     UNIQUE(username)
 );
 
 CREATE TABLE role(
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id INTEGER NOT NULL,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY(id),
     UNIQUE(name)
 );
 
+/* Sport: Fotball, langrenn etc. */
+CREATE TABLE category(
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id),
+    UNIQUE(name)
+);
+
+/* Lag: G13 Fotball, J14 Håndball etc. */
+CREATE TABLE team(
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    category_id INTEGER NOT NULL,
+    gender CHAR(1) NOT NULL,
+    year CHAR(2) NOT NULL,
+    PRIMARY KEY(id) NOT NULL,
+    FOREIGN KEY(category_id) REFERENCES category(id),
+    UNIQUE(name)
+);
+
+/* Aktiviteter som vises i kalenderen */
 CREATE TABLE event(
-    id INTEGER AUTO_INCREMENT,
-    t1 TIMESTAMP NOT NULL,
-    t2 TIMESTAMP NOT NULL,
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    team_id INTEGER NOT NULL,
+    t1 TIMESTAMP NOT NULL, /* starttid */
+    t2 TIMESTAMP NOT NULL, /* slutttid */
     description VARCHAR(100) NOT NULL,
-    event_date DATETIME NOT NULL,
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
+    FOREIGN KEY(team_id) REFERENCES team(id)
+);
+
+/* Fasilitet */
+CREATE TABLE facility(
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    field_name VARCHAR(50) NOT NULL,
+    responsible VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(leader_id) REFERENCES leader(id)
 );
