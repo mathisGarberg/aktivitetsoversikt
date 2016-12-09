94394a0f602b8dbb24fbc6249a77cb84ae781b74
Geir Marius Kirkeberg Jansson <marius.kija@gmail.com>
2016-12-09 20:23:26 +0100

    fix HTML and CSS structure for people.vue

--- app/client/components/People.vue
+++ app/client/components/People.vue
@@ -6,16 +6,18 @@
 
 <template>
   <main class="people">
-    <div class="left-sidebar">
+    <aside class="left-sidebar">
       <div class="team-search">
-        <h3>Lagoversikt</h3>
-        <p>Få oversikt over alle lagene</p>
+        <header>
+          <h3>Lagoversikt</h3>
+          <p>Få oversikt over alle lagene</p>
+        </header>
 
         <form>
-          <input type="text" placeholder="Search"/>
+          <input type="text" placeholder="Søk etter lag"/>
         </form>
 
-        <div id="results">
+        <div class="results">
           <ul>
             <li>Søkeresultat</li>
             <li class="selected">Gutter 18, G18 (Valgt)</li>
@@ -25,87 +27,95 @@
           </ul>
         </div>
       </div>
-    </div>
+    </aside>
     <div class="center-content">
-      <div class="trainer-information">
-        <div class="title">Trenere og informasjon</div>
-        <table>
-          <tr>
-            <td>Rolle:</td>
-            <td>Trener, G18</td>
-          </tr>
-          <tr>
-            <td>Navn:</td>
-            <td>Mathis</td>
-          </tr>
-          <tr>
-            <td>Etternavn:</td>
-            <td>Garberg</td>
-          </tr>
-          <tr>
-            <td>E-post:</td>
-            <td>mathis.garberg@gmail.com</td>
-          </tr>
-          <tr>
-            <td>Telefon:</td>
-            <td>98475632</td>
-          </tr>
-        </table>
-        <div class="button">KONTAKT</div>
-      </div>
-      <div class="main-box">
-        <div class="title">Hovedfasiliteter</div>
-        <table>
-          <tr>
-            <td>Banenavn:</td>
-            <td>Vidarvoll</td>
-          </tr>
-          <tr>
-            <td>Ansvarlig:</td>
-            <td>Petter Ansvarlig</td>
-          </tr>
-        </table>
+      <div class="top-boxes">
+        <div class="trainer-information">
+          <div class="title">Trenere og informasjon</div>
+          <div class="box-content">
+            <table>
+              <tr>
+                <td>Rolle:</td>
+                <td>Trener, G18</td>
+              </tr>
+              <tr>
+                <td>Navn:</td>
+                <td>Mathis</td>
+              </tr>
+              <tr>
+                <td>Etternavn:</td>
+                <td>Garberg</td>
+              </tr>
+              <tr>
+                <td>E-post:</td>
+                <td><a href="mailto:mathis.garberg@gmail.com">mathis.garberg@gmail.com</a></td>
+              </tr>
+              <tr>
+                <td>Telefon:</td>
+                <td><a href="tel:+4798475632">98 47 56 32</a></td>
+              </tr>
+            </table>
+            <a href="mailto:mathis.garberg@gmail.com" class="button">KONTAKT</a>
+          </div>
+        </div>
+        <div class="main-box">
+          <div class="title">Hovedfasiliteter</div>
+          <div class="box-content">
+            <table>
+              <tr>
+                <td>Banenavn:</td>
+                <td>Vidarvoll</td>
+              </tr>
+              <tr>
+                <td>Ansvarlig:</td>
+                <td>Petter Ansvarlig</td>
+              </tr>
+            </table>
+          </div>
+        </div>
       </div>
       <div class="player-information">
         <div class="title">Spiller informasjon</div>
-        <div class="profile-image">
-          <img src="/public/img/profile.png"/>
+        <div class="box-content">
+          <div class="profile-image">
+            <img src="/public/img/profile.png"/>
+          </div>
+          <table>
+            <tr>
+              <td>Fornavn:</td>
+              <td>Petter</td>
+            </tr>
+            <tr>
+              <td>Etternavn:</td>
+              <td>Husrom</td>
+            </tr>
+            <tr>
+              <td>Alder:</td>
+              <td>18</td>
+            </tr>
+            <tr>
+              <td>Født:</td>
+              <td>21.10.1998</td>
+            </tr>
+            <tr>
+              <td>Rolle:</td>
+              <td>Midtbane</td>
+            </tr>
+            <tr>
+              <td>E-post:</td>
+              <td>petter.husrom@gmail.com</td>
+            </tr>
+          </table>
         </div>
-        <table>
-          <tr>
-            <td>Fornavn:</td>
-            <td>Petter</td>
-          </tr>
-          <tr>
-            <td>Etternavn:</td>
-            <td>Husrom</td>
-          </tr>
-          <tr>
-            <td>Alder:</td>
-            <td>18</td>
-          </tr>
-          <tr>
-            <td>Født:</td>
-            <td>21.10.1998</td>
-          </tr>
-          <tr>
-            <td>Rolle:</td>
-            <td>Midtbane</td>
-          </tr>
-          <tr>
-            <td>E-post:</td>
-            <td>petter.husrom@gmail.com</td>
-          </tr>
-        </table>
       </div>
     </div>
-    <div class="right-sidebar">
+    <aside class="right-sidebar">
       <div class="title">Spillere</div>
 
       <form>
         <input type="text" name="" id="" placeholder="Søk etter navn"/>
       </form>
-      <div id="results">
+      <div class="results">
         <ul>
           <li>Navn resultat</li>
           <li>Navn resultat</li>
@@ -118,176 +128,154 @@
           <li>Navn resultat</li>
         </ul>
       </div>
-    </div>
+    </aside>
   </main>
 </template>
 
 <style lang="sass">
+  .people {
+    background: #333;
+  }
+
   .left-sidebar {
-    float: left;
-    width: 15%;
-    background: #1F1F1F;
-    height: 100%;
-    padding: 10px;
+    padding: 0 1rem;
 
     & .team-search {
-      color: #fff;
 
-      & h3 {
-        color: #fff;
-        margin: 0;
-      }
+      & header {
+        height: 4rem;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
 
-      & p {
-        margin-top: 0;
+        & h3 {
+          margin: 0;
+        }
+
+        & p {
+          margin: 0;
+        }
+
       }
 
       & input[type=text] {
+        padding: .5rem;
         width: 100%;
-        border-radius: 5px;
-        padding: 10px;
-        height: 30px;
-        border: 0px;
+        border: none;
+        box-sizing: border-box;
+        font: inherit;
+      }
+    }
+  }
+
+  .results {
+    background: #333;
+
+    & ul {
+      padding: .5rem;
+      list-style: none;
+
+      & li {
+        padding: .5rem;
+      }
+
+      & .selected {
+        color: var(--theme-red);
       }
     }
   }
 
   .right-sidebar {
-    float: left;
-    height: 100%;
-    background: #1F1F1F;
-    width: 20%;
-    padding: 10px;
-    box-sizing: border-box;
+    padding: 1rem;
 
-    & ul {
-      padding: 0;
+    & .results {
+      background: none;
 
-      & li {
-        display: inline;
-        padding: 10px;
+      & ul {
+        padding: 0;
       }
     }
 
     & input[type=text] {
+      padding: .5rem;
       width: 100%;
-      border-radius: 5px;
-      height: 30px;
-      padding: 10px;
-      border: 0px;
-    }
-  }
-
-  #results {
-    background: #555555;
-    padding: 10px;
-    color: #fff;
-
-    & ul {
-      padding: 0;
-
-      & :-first-child {
-        display: block;
-        padding: 0px;
-      }
-
-      & li {
-        display: block;
-        padding: 5px;
-      }
-
-      & .selected {
-        color: red;
-      }
+      border: none;
+      box-sizing: border-box;
+      font: inherit;
     }
   }
 
   .center-content {
-    width: 63%;
-    height: 100%;
-    float: left;
-    padding: 10px;
-    box-sizing: border-box;
-    overflow-y: scroll;
+    flex: auto;
+    padding: 1rem;
+    overflow-y: auto;
+    display: flex;
+    flex-direction: column;
 
     & .title {
-      width: 100%;
-      padding: 10px;
-      background: #FA5952;
+      padding: .5rem;
+      background: var(--theme-red);
       box-sizing: border-box;
-      color: #fff;
-      margin-bottom: 10px;
     }
 
-    & .trainer-information {
-      width: 49%;
-      height: 400px;
-      background: #1F1F1F;
-      float: left;
+    & .box-content {
+      padding: 1rem;
+    }
 
-      & table {
-        color: #fff;
-        padding: 10px;
+    & .top-boxes {
+      display: flex;
 
-        & td {
-          padding: 10px;
+      & .trainer-information {
+        flex: 1 1 0;
+        background: #1F1F1F;
+        overflow: auto;
+
+        & table td {
+          padding: .5rem;
+        }
+
+        & a {
+          color: inherit;
         }
       }
 
       & .button {
-        height: 50px;
-        width: 150px;
-        line-height: 50px;
+        padding: 1rem;
         text-align: center;
         background: #129384;
-        margin-left: 20px;
-        color: #fff;
-        border-radius: 5px;
+        border-radius: .25rem;
+        color: inherit;
+        text-decoration: none;
+        display: inline-block;
       }
     }
 
     & .main-box {
-      width: 49%;
-      height: 400px;
+      margin-left: 1rem;
+      flex: 1 1 0;
       background: #1F1F1F;
-      float: right;
-
-      & table {
-        padding: 10px;
-        color: #fff;
-      }
+      overflow: auto;
 
       & table td {
-        padding: 10px;
+        padding: .5rem;
       }
     }
   }
 
   .player-information{
-    width: 100%;
-    height: auto;
     background: #1F1F1F;
-    float: left;
-    margin-top: 15px;
-
-    & table {
-      padding: 10px;
-      color: #fff;
-    }
+    margin-top: 1rem;
+    overflow: auto;
 
     & table td {
-      padding: 10px;
+      padding: .5rem;
     }
 
     & .profile-image {
-      width: 100px;
-      height: 100px;
-      margin-left: 20px;
-      text-align: center;
 
       & img {
-        max-width: 100%;
-        max-height: 100%;
+        width: 8rem;
+        height: 8rem;
       }
     }
   }
